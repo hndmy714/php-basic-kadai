97e68133d9618cce84b9ee5ecfbbd28eb2c701a1
hndmy714 <mayu.h.928714@gmail.com>
2024-10-05 22:53:21 +0900

    課題提出

---
+++ kadai_sort/sort.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        function sort_2way($array, $order) {
+            if ($order === true) {
+                sort($array);
+            }else
+                rsort($array);
+            foreach ($array as $num) {
+                echo $num . '<br>';
+            }
+        }
+
+        $num = [15, 4, 18, 23, 10];
+
+        echo '昇順でソートします。<br>';
+        echo sort_2way($num, true);
+
+        echo '降順でソートします。<br>';
+        echo sort_2way($num, false);
+        ?>
+    </p>
+</body>
+
+</html>
+ 
